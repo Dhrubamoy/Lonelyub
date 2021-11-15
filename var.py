8aeaed8e763601a6a10cc3ff63e6f036238f7809
LEGEND-OS <Krishna045Jaiswal@gmail.com>
2021-11-15 01:54:46 +0000

    pylint: auto fixes

--- var.py
+++ var.py
@@ -43,6 +43,7 @@ class Var(object):
             raise ValueError(
                 "Invalid Private Group ID. Make sure your ID is starts with -100 and make sure that it is only numbers."
             )
-            
+
+
 class Development(Var):
     LOGGER = True
