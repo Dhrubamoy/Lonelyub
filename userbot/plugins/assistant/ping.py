88ce24fe5ff475f80f3c46eee56d4e76c062ea6c
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-16 03:31:15 +0530

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -1,49 +1,17 @@
-# Copyright (C) Midhun KM
-#
-# Please Don't Kang Without Credits
-# A Plugin For Assistant Bot
-# x0x
-
+import asyncio
+import os
 from datetime import datetime
 
 from telethon import events
 
-from userbot import *
-from userbot import ALIVE_NAME
-from userbot.plugins import *
-from userbot.utils import *
+
+from .. import ALIVE_NAME
+from ..cmdhelp import CmdHelp
+from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 LEGEND_IMG = os.environ.get(
     "BOT_PING_PIC", "https://telegra.ph/file/a9f6a3c160977352dd595.jpg"
 )
-
-
-def get_readable_time(seconds: int) -> str:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
 
 
 @tgbot.on(events.NewMessage(pattern="^/ping"))
