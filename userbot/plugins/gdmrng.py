551d5b68553377f9bf5881af2d3644fbb0dde70d
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-16 08:13:20 +0530

    Update gdmrng.py

--- userbot/plugins/gdmrng.py
+++ userbot/plugins/gdmrng.py
@@ -1,7 +1,7 @@
 from . import *
 
 
-"""@bot.on(admin_cmd(pattern="gdmrng(.*)"))
+@bot.on(admin_cmd(pattern="gdmrng1(.*)"))
 async def xd(event):
     await event.edit("Sending To all Group good Morning")
     event.pattern_match.group(1)
@@ -20,7 +20,7 @@ async def xd(event):
                 lol += 1
     await event.reply(
         f"I hope your morning is as bright and gorgeous as your smile.[Lêɠêɳ̃dẞø†](https://t.me/Official_LegendBot)"
-    )"""
+    )
 
 
 @bot.on(admin_cmd(pattern="gdmrng(.*)"))
@@ -35,7 +35,7 @@ async def xd(event):
             try:
                 await bot.send_message(
                     chat,
-                    f"G🌷o🍃o🌷D\nM🍃o🌷r🍃N🌷i🍃N🌷g\n\n           No matter how good or \nbad your life is,\nwake up each morning\nand be thankful.\nYou still have a new day.\n\n        🌞  \n \n         ╱◥████◣\n│田│▓ ∩│◥███◣\n╱◥◣ ◥████◣田∩田│\n│╱◥█◣║∩∩∩ 田∩田│\n║◥███◣∩田∩ 田∩田│\n│∩│ ▓ ║∩田│║▓田▓\n🌹🌷🌹🌷🌹🍃🌷🌹🌷🌹\n"
+                    f"G🌷o🍃o🌷D\nM🍃o🌷r🍃N🌷i🍃N🌷g\n\nNo matter how good or \nbad your life is,\nwake up each morning\nand be thankful.\nYou still have a new day.\n\n        🌞  \n \n         ╱◥████◣\n│田│▓ ∩│◥███◣\n╱◥◣ ◥████◣田∩田│\n│╱◥█◣║∩∩∩ 田∩田│\n║◥███◣∩田∩ 田∩田│\n│∩│ ▓ ║∩田│║▓田▓\n🌹🌷🌹🌷🌹🍃🌷🌹🌷🌹\n"
                 )
                 done += 1
             except:
@@ -46,4 +46,12 @@ async def xd(event):
 
 CmdHelp("gm").add_command(
     "gdmrng", None, "Wishs Good moning in all groups just one command"
+).add_command(
+    "gdmrng1", None, "Wish Good Morning To All"
+).add_info(
+    "Good Morning Wish Command"
+).add_warning(
+    "Harmless Module✅"
+).add_type(
+    "Addons"
 ).add()
