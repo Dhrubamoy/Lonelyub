6f8f41998f5bc59308638850415ddbc5c219191f
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-16 10:23:05 +0530

    Update _core.py

--- userbot/plugins/_core.py
+++ userbot/plugins/_core.py
@@ -37,7 +37,7 @@ async def send(event):
             allow_cache=False,
             reply_to=message_id,
         )
-    elif os.path.exists(the_1plugin_file):
+    if os.path.exists(the_1plugin_file):
         lauda2 = await event.client.send_file(
             event.chat_id,
             the_1plugin_file,
@@ -48,7 +48,7 @@ async def send(event):
             reply_to=message_id,
         )
         await event.delete()
-    elif os.path.exists(the_2plugin_file):
+    if os.path.exists(the_2plugin_file):
         lauda3 = await event.client.send_file(
             event.chat_id,
             the_2plugin_file,
@@ -59,7 +59,7 @@ async def send(event):
             reply_to=message_id,
         )
         await event.delete()
-    elif os.path.exists(the_3plugin_file):
+    if os.path.exists(the_3plugin_file):
         lauda4 = await event.client.send_file(
             event.chat_id,
             the_3plugin_file,
