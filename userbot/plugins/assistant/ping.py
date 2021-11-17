ae3501813371848fe00e11097e3d538c23526ad0
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-17 18:27:41 +0530

    Update ping.py

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from userbot import *
 from userbot.plugins import *
+from userbot.Config import Config 
 
 LEGEND_IMG = os.environ.get(
     "BOT_PING_PIC", "https://telegra.ph/file/a9f6a3c160977352dd595.jpg"
@@ -16,6 +17,6 @@ async def _(event):
     ms = (end - start).microseconds / 1000
     if LEGEND_IMG:
         legend_caption = (
-            f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ {ms}\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{ALIVE_NAME}』"
+            f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ 4\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{Config.ALIVE_NAME}』"
         )
         await tgbot.send_message(event.chat_id, LEGEND_IMG, caption=legend_caption)
