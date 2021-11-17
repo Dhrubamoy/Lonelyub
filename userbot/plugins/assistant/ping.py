d75966942c31df8750f3bd98df1a48641dd9c431
LEGEND-OS <LEGEND-OS@users.noreply.github.com>
2021-11-17 12:58:27 +0000

    pylint: auto fixes

--- userbot/plugins/assistant/ping.py
+++ userbot/plugins/assistant/ping.py
@@ -2,8 +2,8 @@ import os
 from datetime import datetime
 
 from userbot import *
+from userbot.Config import Config
 from userbot.plugins import *
-from userbot.Config import Config 
 
 LEGEND_IMG = os.environ.get(
     "BOT_PING_PIC", "https://telegra.ph/file/a9f6a3c160977352dd595.jpg"
@@ -14,7 +14,7 @@ LEGEND_IMG = os.environ.get(
 async def _(event):
     start = datetime.now()
     end = datetime.now()
-    ms = (end - start).microseconds / 1000
+    (end - start).microseconds / 1000
     if LEGEND_IMG:
         legend_caption = (
             f"**꧁•⊹٭Ping٭⊹•꧂**\n\n   ⚜ 4\n   ⚜ ❝𝐌𝐲 𝐌𝐚𝐬𝐭𝐞𝐫❞ ~『{Config.ALIVE_NAME}』"
