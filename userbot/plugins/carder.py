105e4395276c0212201cece95d4558388bd74775
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-11-18 17:11:42 +0530

    Update carder.py

--- userbot/plugins/carder.py
+++ userbot/plugins/carder.py
@@ -29,25 +29,25 @@ async def _(event):
     if event.fwd_from:
         return
     LEGEND_input = event.pattern_match.group(1)
-    chat = "@carol5_bot"
+    chat = "@szbinscheckerbot"
     await event.edit("Checking...")
     async with event.client.conversation(chat) as conv:
         try:
             response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1247032902)
+                events.NewMessage(incoming=True, from_users=2143004427)
             )
             await event.client.send_message(chat, f"/bin {LEGEND_input}")
             response = await response
         except YouBlockedUserError:
-            await event.reply("Please Unblock @carol5_bot")
+            await event.reply("Please Unblock @szbinscheckerbot")
             return
         else:
             await event.delete()
             await event.client.send_message(event.chat_id, response.message)
 
 
-@LEGENDBOT.on(admin_cmd(pattern="vbv ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="vbv ?(.*)", allow_sudo=True))
+@LEGENDBOT.on(admin_cmd(pattern="register ?(.*)"))
+@LEGENDBOT.on(sudo_cmd(pattern="register ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -59,7 +59,7 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1247032902)
             )
-            await event.client.send_message(chat, f"/vbv {LEGEND_input}")
+            await event.client.send_message(chat, f"/register {LEGEND_input}")
             response = await response
         except YouBlockedUserError:
             await event.reply("Please Unblock @carol5_bot")
@@ -69,8 +69,9 @@ async def _(event):
             await event.client.send_message(event.chat_id, response.message)
 
 
-@LEGENDBOT.on(admin_cmd(pattern="key ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="key ?(.*)", allow_sudo=True))
+
+@LEGENDBOT.on(admin_cmd(pattern="password ?(.*)"))
+@LEGENDBOT.on(sudo_cmd(pattern="password ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -82,76 +83,7 @@ async def _(event):
             response = conv.wait_event(
                 events.NewMessage(incoming=True, from_users=1247032902)
             )
-            await event.client.send_message(chat, f"/key {LEGEND_input}")
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Please Unblock @carol5_bot")
-            return
-        else:
-            await event.delete()
-            await event.client.send_message(event.chat_id, response.message)
-
-
-@LEGENDBOT.on(admin_cmd(pattern="iban ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="iban ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    LEGEND_input = event.pattern_match.group(1)
-    chat = "@carol5_bot"
-    await event.edit("Checking...")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1247032902)
-            )
-            await event.client.send_message(chat, f"/iban {LEGEND_input}")
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Please Unblock @carol5_bot")
-            return
-        else:
-            await event.delete()
-            await event.client.send_message(event.chat_id, response.message)
-
-
-@LEGENDBOT.on(admin_cmd(pattern="ccheck ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="ccheck ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    LEGEND_input = event.pattern_match.group(1)
-    chat = "@carol5_bot"
-    await event.edit("Checking...")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1247032902)
-            )
-            await event.client.send_message(chat, f"/ss {LEGEND_input}")
-            response = await response
-        except YouBlockedUserError:
-            await event.reply("Please Unblock @carol5_bot")
-            return
-        else:
-            await event.delete()
-            await event.client.send_message(event.chat_id, response.message)
-
-
-@LEGENDBOT.on(admin_cmd(pattern="ccbin ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="ccbin ?(.*)", allow_sudo=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    LEGEND_input = event.pattern_match.group(1)
-    chat = "@carol5_bot"
-    await event.edit(f"Trying to generate CC from the given bin `{LEGEND_input}`")
-    async with event.client.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1247032902)
-            )
-            await event.client.send_message(chat, f"/gen {LEGEND_input}")
+            await event.client.send_message(chat, f"/password {LEGEND_input}")
             response = await response
         except YouBlockedUserError:
             await event.reply("Please Unblock @carol5_bot")
@@ -162,15 +94,7 @@ async def _(event):
 
 
 CmdHelp("carder").add_command("gencc", None, "Generates fake cc...").add_command(
-    "ccheck", "<query>", "Checks that the given cc is live or not"
+    "register", None, "Register Ur Account Here"
 ).add_command(
-    "iban", "<query>", "Checks that the given IBAN ID is live or not"
-).add_command(
-    "key", "<query>", "Checks the status of probided key"
-).add_command(
-    "vbv", "<query>", "Checks the vbv status of given card"
-).add_command(
-    "bin", "<query>", "Checks that the given bin is valid or not"
-).add_command(
-    "ccbin", "<bin>", "Generates CC from the given bin."
+    "password", "<enter>", "Set ur Account Password On CXM.CARDS"
 ).add()
