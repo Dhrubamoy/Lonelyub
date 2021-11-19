f6add6deb78268f7334475f99f0368c0b3da9f63
Akki ThePro <73880884+Akshat7678@users.noreply.github.com>
2021-11-19 19:11:59 +0530

    Update k.py

--- userbot/k.py
+++ userbot/k.py
@@ -64,7 +64,7 @@ CUSTOM_AFK = os.environ.get("CUSTOM_AFK", None)
 
 # Upstream Repo
 UPSTREAM_REPO_URL = os.environ.get(
-    "UPSTREAM_REPO_URL", "https://github.com/LEGEND-OS/LEGENDUSERBOT.git"
+    "UPSTREAM_REPO_URL", "https://github.com/Dhrubamoy/Lonelyup.git"
 )
 
 # Last.fm Module
